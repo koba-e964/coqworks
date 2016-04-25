1987e0ec4be66bb5cc0f25e4c376ff5e202bb331
H Koba <koba-e964@users.noreply.github.com>
2016-04-26 02:41:42 +0900

    Add classical logic

--- prop_logic.v
+++ prop_logic.v
@@ -29,8 +29,23 @@ Inductive int_deriv: con -> fml -> Set :=
   | id_be: forall g s, int_deriv g fml_bot -> int_deriv g s
   | id_ni: forall g s, int_deriv (con_cons g s) fml_bot -> int_deriv g (fml_not s)
 .
+Inductive classic_deriv: con -> fml -> Set :=
+  | cd_var: forall g s, var g s -> classic_deriv g s
+  | cd_ie: forall g s t, classic_deriv g (fml_imp s t) -> classic_deriv g s -> classic_deriv g t
+  | cd_ii: forall g s t, classic_deriv (con_cons g s) t -> classic_deriv g (fml_imp s t)
+  | cd_ae1: forall g s t, classic_deriv g (fml_and s t) -> classic_deriv g s
+  | cd_ae2: forall g s t, classic_deriv g (fml_and s t) -> classic_deriv g t
+  | cd_ai : forall g s t, classic_deriv g s -> classic_deriv g t -> classic_deriv g (fml_and s t)
+  | cd_oe: forall g s t u, classic_deriv (con_cons g s) u -> classic_deriv (con_cons g t) u -> classic_deriv (con_cons g (fml_or s t)) u
+  | cd_oi1: forall g s t, classic_deriv g s -> classic_deriv g (fml_or s t)
+  | cd_oi2: forall g s t, classic_deriv g t -> classic_deriv g (fml_or s t)
+  | cd_bi: forall g s, classic_deriv g s -> classic_deriv g (fml_not s) -> classic_deriv g fml_bot
+  | cd_be: forall g s, classic_deriv g fml_bot -> classic_deriv g s
+  | cd_ni: forall g s, classic_deriv (con_cons g s) fml_bot -> classic_deriv g (fml_not s)
+  | cd_ne: forall g s, classic_deriv (con_cons g (fml_not s)) fml_bot -> classic_deriv g s.
 
 Definition is_id_derivable (f: fml): Prop := exists dt: int_deriv con_empty f, True.
+Definition is_cd_derivable (f: fml): Prop := exists dt: classic_deriv con_empty f, True.
 
 Goal is_id_derivable (fml_not fml_bot).
 
@@ -42,6 +57,65 @@ apply var_zero.
 exists H.
 auto.
 Qed.
+
+Goal forall s, int_deriv con_empty (fml_imp s (fml_not (fml_not s))).
+intro s.
+apply id_ii.
+apply id_ni.
+apply (id_bi _ s).
+apply id_var.
+apply var_succ.
+apply var_zero.
+apply id_var.
+apply var_zero.
+Defined.
+
+
+Definition id_doubleneg {g s} (tr: int_deriv g s): int_deriv g (fml_not (fml_not s)).
+apply id_ni.
+apply (id_bi _ s).
+admit.
+apply id_var.
+apply var_zero.
+Admitted.
+
+Fixpoint id_to_cd {g s} (id: int_deriv g s): classic_deriv g s.
+destruct id.
+apply (cd_var _ _ v).
+apply (cd_ie _ s); apply id_to_cd; auto.
+apply (cd_ii _ s); apply id_to_cd; auto.
+apply (cd_ae1 _ s t); apply id_to_cd; auto.
+apply (cd_ae2 _ s t); apply id_to_cd; auto.
+apply (cd_ai _ s t); apply id_to_cd; auto.
+apply (cd_oe _ s t); apply id_to_cd; auto.
+apply (cd_oi1 _ s t); apply id_to_cd; auto.
+apply (cd_oi2 _ s t); apply id_to_cd; auto.
+apply (cd_bi _ s); apply id_to_cd; auto.
+apply (cd_be _ s); apply id_to_cd; auto.
+apply (cd_ni _ s); apply id_to_cd; auto.
+Defined.
+
+Theorem id_then_cd: forall s, is_id_derivable s -> is_cd_derivable s.
+intros s H.
+destruct H as [tr _].
+exists (id_to_cd tr).
+auto.
+Qed.
+
+Fixpoint cd_to_doubleneg_id {g s} (cd: classic_deriv g s): int_deriv g (fml_not (fml_not s)).
+destruct cd.
+apply id_doubleneg.
+apply id_var; auto.
+Admitted.
+
+Theorem cd_then_doubleneg_id: forall s, is_cd_derivable s -> is_id_derivable (fml_not (fml_not s)).
+intros s H.
+destruct H as [tr _].
+exists (cd_to_doubleneg_id tr).
+auto.
+Qed.
+
+
 
 Theorem disjunctive_property: forall s t, is_id_derivable (fml_or s t) -> is_id_derivable s \/ is_id_derivable t.
 intros s t H.
