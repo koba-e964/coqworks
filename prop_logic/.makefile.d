9a11fc324156580e34d61cf64e46b2ff51ff161f
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-04-02 13:37:55 +0900

    Add Makefile

---
+++ prop_logic/.makefile.d
@@ -0,0 +1,36 @@
+fml.vo fml.glob fml.v.beautified fml.required_vo: fml.v 
+fml.vio: fml.v 
+fml.vos fml.vok fml.required_vos: fml.v 
+fml_examples.vo fml_examples.glob fml_examples.v.beautified fml_examples.required_vo: fml_examples.v fml.v
+fml_examples.vio: fml_examples.v fml.v
+fml_examples.vos fml_examples.vok fml_examples.required_vos: fml_examples.v fml.v
+hipc.vo hipc.glob hipc.v.beautified hipc.required_vo: hipc.v fml.vo
+hipc.vio: hipc.v fml.vio
+hipc.vos hipc.vok hipc.required_vos: hipc.v fml.vos
+hipc_examples.vo hipc_examples.glob hipc_examples.v.beautified hipc_examples.required_vo: hipc_examples.v fml.vo hipc.vo
+hipc_examples.vio: hipc_examples.v fml.vio hipc.vio
+hipc_examples.vos hipc_examples.vok hipc_examples.required_vos: hipc_examples.v fml.vos hipc.vos
+natded.vo natded.glob natded.v.beautified natded.required_vo: natded.v fml.vo
+natded.vio: natded.v fml.vio
+natded.vos natded.vok natded.required_vos: natded.v fml.vos
+natded_ext.vo natded_ext.glob natded_ext.v.beautified natded_ext.required_vo: natded_ext.v fml.vo natded.vo
+natded_ext.vio: natded_ext.v fml.vio natded.vio
+natded_ext.vos natded_ext.vok natded_ext.required_vos: natded_ext.v fml.vos natded.vos
+nj.vo nj.glob nj.v.beautified nj.required_vo: nj.v fml.vo natded.vo
+nj.vio: nj.v fml.vio natded.vio
+nj.vos nj.vok nj.required_vos: nj.v fml.vos natded.vos
+nj_examples.vo nj_examples.glob nj_examples.v.beautified nj_examples.required_vo: nj_examples.v nj.vo
+nj_examples.vio: nj_examples.v nj.vio
+nj_examples.vos nj_examples.vok nj_examples.required_vos: nj_examples.v nj.vos
+nj_theorems.vo nj_theorems.glob nj_theorems.v.beautified nj_theorems.required_vo: nj_theorems.v natded_ext.vo nj.vo
+nj_theorems.vio: nj_theorems.v natded_ext.vio nj.vio
+nj_theorems.vos nj_theorems.vok nj_theorems.required_vos: nj_theorems.v natded_ext.vos nj.vos
+nk.vo nk.glob nk.v.beautified nk.required_vo: nk.v fml.vo natded.vo
+nk.vio: nk.v fml.vio natded.vio
+nk.vos nk.vok nk.required_vos: nk.v fml.vos natded.vos
+nk_theorems.vo nk_theorems.glob nk_theorems.v.beautified nk_theorems.required_vo: nk_theorems.v natded_ext.vo nk.vo
+nk_theorems.vio: nk_theorems.v natded_ext.vio nk.vio
+nk_theorems.vos nk_theorems.vok nk_theorems.required_vos: nk_theorems.v natded_ext.vos nk.vos
+nk_to_nj.vo nk_to_nj.glob nk_to_nj.v.beautified nk_to_nj.required_vo: nk_to_nj.v nj_theorems.v natded_ext.vo nj.vo nk_theorems.v natded_ext.vo nk.vo
+nk_to_nj.vio: nk_to_nj.v nj_theorems.v natded_ext.vio nj.vio nk_theorems.v natded_ext.vio nk.vio
+nk_to_nj.vos nk_to_nj.vok nk_to_nj.required_vos: nk_to_nj.v nj_theorems.v natded_ext.vos nj.vos nk_theorems.v natded_ext.vos nk.vos
